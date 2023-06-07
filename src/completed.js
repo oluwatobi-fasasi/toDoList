31e52933f261920a4daf57e7a5af85981e32bfde
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-07 08:53:55 -0400

    Add completed

---
+++ src/completed.js
@@ -0,0 +1,11 @@
+function deleteCompleted() {
+  let toDo = JSON.parse(localStorage.getItem('toDo')) || [];
+  toDo = toDo.filter((todo) => !todo.completed);
+  for (let i = 0; i < toDo.length; i += 1) {
+    toDo[i].num = i + 1;
+  }
+  localStorage.setItem('toDo', JSON.stringify(toDo));
+  window.location.reload();
+}
+
+export default deleteCompleted;
