3d44df0dd39abd9a07cbb76b5dc317ad34dda9ca
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-07 09:06:37 -0400

    Add deleteAll function

--- src/completed.js
+++ src/completed.js
@@ -1,4 +1,4 @@
-const deleteCompleted = () =>{
+const deleteCompleted = () => {
   let toDo = JSON.parse(localStorage.getItem('toDo')) || [];
   toDo = toDo.filter((todo) => !todo.completed);
   for (let i = 0; i < toDo.length; i += 1) {
@@ -6,6 +6,6 @@ const deleteCompleted = () =>{
   }
   localStorage.setItem('toDo', JSON.stringify(toDo));
   window.location.reload();
-}
+};
 
 export default deleteCompleted;
