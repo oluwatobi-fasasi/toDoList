43a0ab2250489fde6e01961640d726a6eb060717
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-14 10:27:55 -0400

    Add add test function

--- src/add.test.js
+++ src/add.test.js
@@ -3,20 +3,31 @@
  */
 
 import './__mocks__/mockHtml.js';
+import { AddToDo } from './add.js';
 
 
 describe('test add ', () => {
   it('Add tasks to local storage', () => {
     
     const input = document.querySelectorAll('list-container');
-    console.log(input);
+    console.log(new AddToDo());
+    const newAddToDo = new AddToDo();
 
-    expect(JSON.parse(localStorage.getItem('array'))).toEqual([
-      { description: 'hello', completed: false, index: 1 },
-      { description: 'start', completed: false, index: 2 },
-      { description: 'middle', completed: false, index: 3 },
-      { description: 'finish', completed: false, index: 4 },
-      { description: 'well-done', completed: false, index: 5 },
+    newAddToDo.addToDo('hello')
+    newAddToDo.addToDo('start')
+    newAddToDo.addToDo('middle')
+    newAddToDo.addToDo('finish')
+    newAddToDo.addToDo('well-done')
+
+    // const ssss = JSON.parse(localStorage.getItem('toDo'))
+    // console.log(ssss)
+
+    expect(JSON.parse(localStorage.getItem('toDo'))).toEqual([
+      { description: 'hello', completed: false, num: 1 },
+      { description: 'start', completed: false, num: 2 },
+      { description: 'middle', completed: false, num: 3 },
+      { description: 'finish', completed: false, num: 4 },
+      { description: 'well-done', completed: false, num: 5 },
     ]);
   });
 });
