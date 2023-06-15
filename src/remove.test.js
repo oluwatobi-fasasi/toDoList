6dbe0eb8b4647992f53f287f403615e9661fe1b3
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-15 04:54:08 -0400

    Add remove test

--- src/remove.test.js
+++ src/remove.test.js
@@ -7,45 +7,41 @@ import './__mocks__/mockHtml.js';
 // import ToDo from './render.js';
 import removeTodo from './remove.js';
 
-describe('test remove', ()=>{
-    it('Remove task', ()=>{
-        const myArray = [];
-        myArray.push(
-            { description: 'hello', completed: false, num: 1 },
-            { description: 'start', completed: false, num: 2 },
-            { description: 'middle', completed: false, num: 3 },
-            { description: 'finish', completed: false, num: 4 },
-            { description: 'well-done', completed: false, num: 5 },
-          )
-          removeTodo(myArray, 2);
+describe('test remove', () => {
+  it('Remove task', () => {
+    const myArray = [];
+    myArray.push(
+      { description: 'hello', completed: false, num: 1 },
+      { description: 'start', completed: false, num: 2 },
+      { description: 'middle', completed: false, num: 3 },
+      { description: 'finish', completed: false, num: 4 },
+      { description: 'well-done', completed: false, num: 5 },
+    );
+    removeTodo(myArray, 2);
 
-          expect(myArray).toEqual(
-            [
-                { description: 'hello', completed: false, num: 1 },
-                { description: 'start', completed: false, num: 2 },
-                { description: 'finish', completed: false, num: 3 },
-                { description: 'well-done', completed: false, num: 4 },
-              ]
-          )
-    });
+    expect(myArray).toEqual(
+      [
+        { description: 'hello', completed: false, num: 1 },
+        { description: 'start', completed: false, num: 2 },
+        { description: 'finish', completed: false, num: 3 },
+        { description: 'well-done', completed: false, num: 4 },
+      ],
+    );
+  });
 
-    it('Add tasks one li to html', () => {
-      
-      const myArray = [];
-        myArray.push(
-            { description: 'hello', completed: false, num: 1 },
-            { description: 'start', completed: false, num: 2 },
-            { description: 'middle', completed: false, num: 3 },
-            { description: 'finish', completed: false, num: 4 },
-            { description: 'well-done', completed: false, num: 5 },
-          )
-      const initiallength = myArray.length;
-      console.log(initiallength)
-      removeTodo(myArray, 2);
-      const finallength = myArray.length;
-      console.log(finallength)
+  it('Add tasks one li to html', () => {
+    const myArray = [];
+    myArray.push(
+      { description: 'hello', completed: false, num: 1 },
+      { description: 'start', completed: false, num: 2 },
+      { description: 'middle', completed: false, num: 3 },
+      { description: 'finish', completed: false, num: 4 },
+      { description: 'well-done', completed: false, num: 5 },
+    );
+    const initiallength = myArray.length;
+    removeTodo(myArray, 2);
+    const finallength = myArray.length;
 
-      
-      expect(finallength).toEqual(initiallength - 1);
-    });
-})
+    expect(finallength).toEqual(initiallength - 1);
+  });
+});
