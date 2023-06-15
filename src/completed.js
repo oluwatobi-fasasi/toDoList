f290655e6351b09a7a7ed3756d92b2b400bf2f1a
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-15 09:17:34 -0400

    Update completed.js

--- src/completed.js
+++ src/completed.js
@@ -1,5 +1,4 @@
-const deleteCompleted = () => {
-  let toDo = JSON.parse(localStorage.getItem('toDo')) || [];
+const deleteCompleted = (toDo) => {
   toDo = toDo.filter((todo) => !todo.completed);
   for (let i = 0; i < toDo.length; i += 1) {
     toDo[i].num = i + 1;
